4b3db801f6a35986555033fa74afae0f7bb9bc9d
atulnitrr <atulkumar458@gmail.com>
2019-06-29 23:51:30 +0530

    Materialize set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>It logger</h1>
-    </div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    // init Materialize java script
+    M.AutoInit();
+  });
+  return <div className="App">It logger</div>;
+};
 
 export default App;
