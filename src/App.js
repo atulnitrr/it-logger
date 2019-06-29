8684580e7ada890420fad93970fe7a9de6757ab9
atulnitrr <atulkumar458@gmail.com>
2019-06-30 00:23:20 +0530

    Added logs components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
 const App = () => {
   useEffect(() => {
     // init Materialize java script
     M.AutoInit();
   });
-  return <div className="App">It logger</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
